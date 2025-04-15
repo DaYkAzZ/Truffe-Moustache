ab5d623148a3d4edb5436c912fb65d85157ae406
dk <dayka6601@gmail.com>
2025-04-15 14:39:35 +0200

    arborescence

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+// HOME
+
+import React from "react";
+import Link from "next/link";
+
+export default function page() {
+  return (
+    <div className="m-48 p-48 text-red-400">
+      <h1>hello world</h1>
+    </div>
+  );
+}
