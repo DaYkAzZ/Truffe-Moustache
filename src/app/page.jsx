6214bebc3bb21f4abdfa56390330eabe088a0ec2
dk <dayka6601@gmail.com>
2025-04-16 11:44:38 +0200

    test develop branch

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function page() {
   return (
     <div className="m-48 p-48 text-red-400">
-      <h1>hello world</h1>
+      <h1>Test develop branch</h1>
     </div>
   );
 }
