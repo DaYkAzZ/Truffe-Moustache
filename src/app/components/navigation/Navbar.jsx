5d6a613a94dc07f3f75b50655db012654dbcb838
dk <dayka6601@gmail.com>
2025-04-17 14:18:34 +0200

    :bug: fix navbar

--- src/app/components/navigation/Navbar.jsx
+++ src/app/components/navigation/Navbar.jsx
@@ -10,14 +10,14 @@ export default function Navbar() {
   const pathname = usePathname();
 
   return (
-    <motion.div 
+    <motion.div
       className="bg-white fixed bottom-0 left-0 w-screen p-5 shadow-lg"
       initial={{ y: 100 }}
       animate={{ y: 0 }}
       transition={{ type: "spring", stiffness: 260, damping: 20 }}
     >
       <div className="flex flex-row justify-center items-center">
-        <motion.div 
+        <motion.div
           className="mx-8"
           whileHover={{ scale: 1.2 }}
           whileTap={{ scale: 0.9 }}
@@ -41,7 +41,7 @@ export default function Navbar() {
           </Link>
         </motion.div>
 
-        <motion.div 
+        <motion.div
           className="mx-8"
           whileHover={{ scale: 1.2 }}
           whileTap={{ scale: 0.9 }}
@@ -65,25 +65,25 @@ export default function Navbar() {
           </Link>
         </motion.div>
 
-        <motion.div 
+        <motion.div
           className="mx-8"
           whileHover={{ scale: 1.2 }}
           whileTap={{ scale: 0.9 }}
         >
-          <Link href="/pages/Products">
-            {pathname === "/pages/Products" ? (
+          <Link href="/pages/CoursDressage">
+            {pathname === "/pages/CoursDressage" ? (
               <Image
                 src="/images/icons/cours-active.svg" // Version orange de l'icône
                 width={32}
                 height={32}
-                alt="icône compte active"
+                alt="icône cours dressage"
               />
             ) : (
               <Image
                 src="/images/icons/cours.svg" // Version normale de l'icône
                 width={32}
                 height={32}
-                alt="icône compte"
+                alt="icône cours"
               />
             )}
           </Link>
