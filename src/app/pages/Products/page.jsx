a8e24ff7086282ca4751e49dc72118af69c13267
LeMattheo <m_lecarpentier1@etu-webschoolfactory.fr>
2025-04-18 11:28:42 +0200

    data-imgAdd

--- src/app/pages/Products/page.jsx
+++ src/app/pages/Products/page.jsx
@@ -4,7 +4,7 @@ import ProductDescription from '@/app/components/ui/ProductDescription';
 const page = memo(() => {
   return (
     <div className="min-h-screen w-full max-w-[393px] mx-auto bg-white">
-      <ProductDescription category="Chiens" id={5} />
+      <ProductDescription category="Chiens" id={2}    />
     </div>
   )
 })
